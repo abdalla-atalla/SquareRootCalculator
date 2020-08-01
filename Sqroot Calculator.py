f3fa275a243f32a0a0ad269dd0590b576c3608ce
Abdalla Atalla <abdalla@abdallas-mbp.attlocal.net>
2020-08-01 09:30:25 -0700

    first commit

---
+++ Sqroot Calculator.py
@@ -0,0 +1,40 @@
+"""
+File: newton.py
+Project 6.1
+
+Compute the square root of a number (uses function with loop).
+
+1. The input is a number, or enter/return to halt the
+   input process.
+
+2. The outputs are the program's estimate of the square root
+   using Newton's method of successive approximations, and
+   Python's own estimate using math.sqrt.
+"""
+
+import math
+
+tolerance = 0.000001
+
+def newton(x,estimate = 1.0):
+    estimate = (estimate + x / estimate) / 2
+    
+    difference = abs(x - estimate ** 2)
+    if difference <= tolerance:
+        return estimate
+    else:
+        return newton(x,estimate)
+
+def main():
+    while True:
+        x = input("Enter a positive number or enter/return to quit: ")
+        if x == "":
+            break
+        x = float(x)
+
+        estimate = 1.0
+        print("The program's estimate is", newton(x,estimate))
+        print("Python's estimate is ", math.sqrt(x))
+
+if __name__ == "__main__":
+    main()
