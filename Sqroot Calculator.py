1a584572cafe9c4f068144d2a119fb5ffa2b351c
abdalla-atalla <60201830+abdalla-atalla@users.noreply.github.com>
2020-08-01 09:32:07 -0700

    Update Sqroot Calculator.py

--- Sqroot Calculator.py
+++ Sqroot Calculator.py
@@ -1,16 +1,3 @@
-"""
-File: newton.py
-Project 6.1
-
-Compute the square root of a number (uses function with loop).
-
-1. The input is a number, or enter/return to halt the
-   input process.
-
-2. The outputs are the program's estimate of the square root
-   using Newton's method of successive approximations, and
-   Python's own estimate using math.sqrt.
-"""
 
 import math
 
